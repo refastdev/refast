0b169375148523947cde2c828f56262fb6560458
luoluoqixi <luoqixi@luoqixi.com>
2024-02-01 00:44:51 +0800

    chore: chore project

--- packages/refast/.eslintrc.cjs
+++ packages/refast/.eslintrc.cjs
@@ -1,6 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
+  ignorePatterns: ['dist/**'],
   extends: ['@refastdev/eslint-config'],
   parser: '@typescript-eslint/parser'
 }
